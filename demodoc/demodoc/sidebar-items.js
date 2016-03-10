b0b66918056f455aacc12ce3fb5b10cf34a46ec3
seb-odessa <seb@ukr.net>
2016-03-10 13:15:46 +0200

    Updates documentation from b76795c revision

--- demodoc/demodoc/sidebar-items.js
+++ demodoc/demodoc/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["get_true","This function always returns true # Examples ``` assert!(get_true()); # fn get_true() -> bool { return true } ``` This function will return true"]],"struct":[["Foo","a Foo struct"]],"trait":[["HasName","The trait HasName"]]});
+initSidebarItems({"fn":[["get_true","This function always returns true # Examples ``` assert!(get_true()); # fn get_true() -> bool { return true } ``` This function returns true"]],"struct":[["Foo","a Foo struct"]],"trait":[["HasName","The trait HasName"]]});
