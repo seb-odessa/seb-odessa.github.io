3c8cc6e0a72dc7ceca9fd5a971b15cb0020511fd
seb-odessa <seb@ukr.net>
2016-03-10 12:59:24 +0200

    Updates documentation from b76795c revision

--- demodoc/search-index.js
+++ demodoc/search-index.js
@@ -1,3 +1,3 @@
 var searchIndex = {};
-searchIndex['demodoc'] = {"items":[[0,"","demodoc","Demo library. This is a crate level description",null,null],[3,"Foo","","Foo struct with the some payload",null,null],[5,"get_true","","The function pub fn get_true() -> bool always returns true\nThis function will return true",null,{"inputs":[],"output":{"name":"bool"}}]],"paths":[]};
+searchIndex['demodoc'] = {"items":[[0,"","demodoc","A demo library. This is a crate level description",null,null],[3,"Foo","","Foo struct with the some payload",null,null],[5,"get_true","","This function always returns true\n# Examples",null,{"inputs":[],"output":{"name":"bool"}}]],"paths":[]};
 initSearch(searchIndex);
